1b7887b1e55d44fce577b10e4468acaa792dd1e3
ekov1 <kristian_ekov@abv.bg>
2020-11-22 16:28:46 +0200

    .

--- 03_execution_flow/12. IsPoinInYinYang/12. IsPoinInYinYang.cpp
+++ 03_execution_flow/12. IsPoinInYinYang/12. IsPoinInYinYang.cpp
@@ -1,13 +1,17 @@
 ﻿#include <iostream>
-
+// 00 е в 2те полета
 int main()
 {
 	float x, y;
+	std::cout << "enter point x y : ";
 	std::cin >> x >> y;
 
 	float distFromCenter = sqrt((x * x) + (y * y));
 	if (distFromCenter == 4) {
 		std::cout << "point is on contur";
+	}
+	if (distFromCenter > 4) {
+		std::cout << "point is ouside";
 	}
 
 	// 2ри квадрант
@@ -24,7 +28,51 @@ int main()
 	}
 
 	// 1ви квадрант
+	if ((0 < x && x < 4) && (0 <= y && y < 4)) {
+		// small black circle
+		float distX = abs(0 - x);
+		float distY = abs(2 - y);
+		float distAB = sqrt((distX * distX) + (distY * distY));
+		if (distAB <= 1) {
+			std::cout << "point is in black space";
+		}
+		else if (1 < distAB && distAB <= 2) {
+			std::cout << "point is in white space";
+		}
+		else {
+			std::cout << "point is in black space";
+		}
+	}
 
+	// 3ти квадрант
+	if ((-4 < x && x < 0) && (-4 < y && y < 0)) {
+		// малък бял кръг
+		float distX = abs(0 - x);
+		float distY = abs(-2 - y);
+		float distAB = sqrt((distX * distX) + (distY * distY));
+		if (distAB <= 1) {
+			std::cout << "point is in white space";
+		}
+		else if (1 < distAB && distAB <= 2) {
+			std::cout << "point is in black space";
+		}
+		else {
+			std::cout << "point is in white space";
+		}
+	}
+
+	//4ти квадрант
+	if ((0 <= x && x < 4) && (-4 < y && y <= 0)) {
+		float distX = abs(0 - x);
+		float distY = abs(-2 - y);
+		float distAB = sqrt((distX * distX) + (distY * distY));
+		if (distAB >= 1) {
+			std::cout << "point is in black space";
+		}
+		else {
+			std::cout << "point is in white space";
+		}
+	}
 }
 
 
---
+++ 04_loops/08.  HighestOrderBit/08.  HighestOrderBit.cpp
@@ -0,0 +1,23 @@
+﻿// Напишете програма, която да преброи колко единици и нули
+// има в двоичното представяне на дадено цяло число.
+
+#include <iostream>
+
+int main()
+{
+	unsigned n, remainder;
+	std::cin >> n;
+
+	short i = 31;
+	short bit,position;
+	while (i >= 0)
+	{
+		bit = (n & (1 << i)) >> i;
+		if (bit == 1) {
+			position = i;
+		}
+		
+		i--;
+	}
+	std::cout << "position = " << position ;
+}
